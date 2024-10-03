f349a8f0dbbb60f5e88abe69f32782c4537976c4
jahedripon9 <jahedahmed3170@gmail.com>
2024-10-03 11:31:34 +0600

    add New more Section

---
+++ src/pages/FAQ/FAQ.jsx
@@ -0,0 +1,100 @@
+const FAQ = () => {
+  return (
+    <section className="ezy__faq5 light py-14 md:py-12 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
+      <div className="container px-16 md:px-8 lg:px-28">
+        <div className="grid grid-cols-12 justify-center md:mb-6">
+          <div className="col-span-12 lg:col-span-8 lg:col-start-3 xl:px-12 text-center">
+            <h2 className="font-bold text-[25px] md:text-[45px] leading-none mb-4">
+              Frequently Asked Questions
+            </h2>
+            <p className="">
+              It’s easier to reach your savings goals when you have the right
+              savings account. Take a look and find the right one for you!
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-12 gap-0 md:gap-6">
+          <div className="col-span-12 md:col-span-6">
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>What is Easy Frontend?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>Who is Easy Frontend for?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>How does Easy Frontend work?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6">
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>How often does your team upload resources?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>Can I get a refund if I cancel my subscription?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-100 dark:bg-[#1E2735] mt-6 rounded-xl">
+              <h5 className="p-4 lg:p-6 lg:pb-5 w-full text-start flex justify-between items-center font-medium text-xl">
+                <span>Can I use Easy Frontend designs in my portfolio?</span>
+              </h5>
+              <div className="ezy__faq5-content px-4 lg:px-6 lg:pb-6">
+                <p className="opacity-50 mb-0">
+                  When it comes to booking a holiday, we know everyone likes
+                  something different - so we've designed our getaways with you
+                  in mind.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import OurTeam from "../OurTeam/OurTeam";
 import Footer from "../Shared/Footer";
 import Navbar from "../Shared/Navbar";
 import TestimonialsOne from "../Testimonials/TestimonialsOne";
+import FAQ from "../FAQ/FAQ";
 
 const Home = () => {
   return (
@@ -49,6 +50,7 @@ const Home = () => {
         <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
         <span className="relative z-10 bg-white px-6">Our Services</span>
       </span>
+      <FAQ />
       <Footer />
     </div>
   );
